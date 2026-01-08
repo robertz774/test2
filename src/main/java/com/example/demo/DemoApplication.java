0bc1b43a4f0e1b040a3ea5a5e46b07722c867736
robert <robert774@o2.pl>
2026-01-09 00:08:34 +0100

    add log

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -90,6 +90,7 @@ public class DemoApplication {
             System.out.println("sout pole:1234 sout");
             System.out.println("sout2 pole:1234 sout");
             System.out.println("sout3 pole:1234 sout");
+            System.out.println("sout4 pole:1234 sout");
             return McpSchema.CallToolResult.builder()
                     .structuredContent(new JacksonMcpJsonMapper(new ObjectMapper()),"{\"term1\":\"gitara1\"}")
 //                    .addTextContent("txtCont")
